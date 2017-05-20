31bcdc4e00b0195eb35aef3a3da51b5b1d239f68
adamPrestor <adam.prestor@gmail.com>
2017-05-21 00:43:01 +0200

    AlarmClockInit

--- android_app/ajapp/app/src/main/java/ajateam/ajapp/MainActivity.java
+++ android_app/ajapp/app/src/main/java/ajateam/ajapp/MainActivity.java
@@ -1,29 +1,82 @@
 package ajateam.ajapp;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlarmManager alarmManager;
+    TimePicker alarmTimePicker;
+    TextView updateStatus;
+    Context context;
+    PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        this.context = this;
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmOn);
+        updateStatus = (TextView) findViewById(R.id.updateText);
+
+        final Calendar calendar = Calendar.getInstance();
+
+        final Intent intent = new Intent(this.context, AlarmReceiver.class);
+
+        Button alarmOn = (Button) findViewById(R.id.setAlarm);
+        alarmOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+                calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+
+                int hour = alarmTimePicker.getCurrentHour();
+                int min = alarmTimePicker.getCurrentMinute();
+
+                setAlarmText("Hey ho, let's go " + hour + " hour " + min + " min.");
+
+                intent.putExtra("extra", true);
+
+                pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+
+
+            }
+        });
+
+        Button alarmOff = (Button) findViewById(R.id.cancelAlarm);
+        alarmOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                setAlarmText("Hey oh, oh no.");
+
+                alarmManager.cancel(pendingIntent);
+
+                intent.putExtra("extra", false);
+
+                sendBroadcast(intent);
+            }
+        });
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    private void setAlarmText(String output) {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
+        updateStatus.setText(output);
     }
+
 }
